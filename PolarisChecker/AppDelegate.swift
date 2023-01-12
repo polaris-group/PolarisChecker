87dc10107bbcd9a24e25112f50c07e1004bef7d5
dyljqq <355086587@qq.com>
2023-01-12 11:54:58 +0800

    Initial Commit

---
+++ PolarisChecker/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  PolarisChecker
+//
+//  Created by polaris dev on 2023/1/12.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ PolarisChecker/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PolarisChecker
+//
+//  Created by polaris dev on 2023/1/12.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
